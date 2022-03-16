1d787181036998395b58e729709e6bf057565ff8
cbotek <cbotek@ferlab.bio>
2022-03-16 08:36:53 -0400

    Add medecho anonymized dag

--- dags/ingestion_anonymized_dags.py
+++ dags/ingestion_anonymized_dags.py
@@ -20,6 +20,7 @@ SCHEMAS = [
     ("anonymized", "eclinibase", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
     ("anonymized", "etraceline", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
     ("anonymized", "laboratoire_systeme", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
+    ("anonymized", "medecho", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
     ("anonymized", "pericalm", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
     ("anonymized", "pharmacie", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
     ("anonymized", "softlab", "bio.ferlab.ui.etl.yellow.anonymized.Main"),
