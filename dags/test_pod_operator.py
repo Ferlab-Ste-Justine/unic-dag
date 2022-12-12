42bfc333a70a1289b0f93b958f8a104cbf6c09db
zoemcl <zoemmclennan@gmail.com>
2022-12-12 10:29:47 -0500

    feat: UNIC-875 updating test operator

--- dags/test_pod_operator.py
+++ dags/test_pod_operator.py
@@ -1,20 +1,22 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from operators.spark import SparkOperator
 from datetime import datetime
 
+JAR = "s3a://spark-prd/jars/unic-etl-{{ dag_run.conf.get('branch', 'UNIC-875') }}.jar"
 
 with DAG(
         dag_id='test_pod_operator_default',
         start_date=datetime(2022, 1, 1),
         schedule_interval=None,
 ) as dag:
-
-    test_pod_operator_default = KubernetesPodOperator(
+    test_pod_operator_default = SparkOperator(
         task_id='test_pod_operator_default',
         name='test-pod-operator-default',
-        is_delete_operator_pod=True,
-        namespace='unic-prod',
-        image='alpine',
+        namespace="ingestion",
+        spark_class="bio.ferlab.ui.etl.experimental.TestClass",
+        spark_jar=JAR,
         cmds=['echo', 'hello'],
-        arguments=[],
+        spark_config=f"xsmall-etl",
+        dag=dag
     )
