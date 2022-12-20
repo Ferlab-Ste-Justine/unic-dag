01f674361e44ba818d3654b83eefd8012f371e0d
zoemcl <zoemmclennan@gmail.com>
2022-12-20 16:05:44 -0500

    UNIC-875: changing pod name

--- dags/test_pod_operator.py
+++ dags/test_pod_operator.py
@@ -5,13 +5,13 @@ from datetime import datetime
 JAR = "s3a://spark-prd/jars/unic-etl-UNIC-875.jar"
 
 with DAG(
-        dag_id='test_pod_operator-1',
+        dag_id='test_pod_operator-2',
         start_date=datetime(2022, 1, 1),
         schedule_interval=None,
 ) as dag:
     test_pod_operator = SparkOperator(
-        task_id='test_pod_operator-1',
-        name='test-pod-operator-1',
+        task_id='test_pod_operator-2',
+        name='test-pod-operator-2',
         namespace="raw",
         spark_class="bio.ferlab.ui.etl.experimental.TestClass",
         spark_jar=JAR,
