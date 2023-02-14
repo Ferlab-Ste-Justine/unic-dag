1ad75773aa30e880275398d84149496d2ab7d689
Laura Bégin <lbegin@ferlab.bio>
2023-02-14 13:20:29 -0500

    fix: UNIC-875 Apply Pylint suggestions (#169)

--- dags/schema_diff_checker.py
+++ dags/schema_diff_checker.py
@@ -1,22 +1,26 @@
-from airflow import DAG
-from core.slack import Slack
+"""
+DAG : checks if there are new tables available in the Centro schema in the integration db
+"""
 from datetime import datetime, timedelta
-from airflow.models.param import Param
-from operators.spark import SparkOperator
-from core.default_args import generate_default_args
 
+from airflow import DAG
+from airflow.models.param import Param
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+
+from core.default_args import generate_default_args
+from core.slack import Slack
+from operators.spark import SparkOperator
 
 args = ["config/prod.conf", "initial", "dbschema"]
 
-namespace = "raw"
-pod_name = "raw-schema-diff-checker"
+NAMESPACE = "raw"
+POD_NAME = "raw-schema-diff-checker"
 
-main_class = "bio.ferlab.ui.etl.script.SchemaDiffChecker"
+MAIN_CLASS = "bio.ferlab.ui.etl.script.SchemaDiffChecker"
 
-jar = 's3a://spark-prd/jars/unic-etl-{{ params.branch }}.jar'
+JAR = 's3a://spark-prd/jars/unic-etl-{{ params.branch }}.jar'
 
-default_args = generate_default_args(owner="jamine", on_failure_callback=Slack.notify_task_failure)
+default_args = generate_default_args(owner="unic", on_failure_callback=Slack.notify_task_failure)
 
 slack = Slack()
 
@@ -30,29 +34,37 @@ dag = DAG(
     },
     dagrun_timeout=timedelta(hours=1),
     default_args=default_args,
-    tags=["dbschema"],
+    tags=["script"],
     is_paused_upon_creation=True,
     catchup=False,
 )
 
 check_schema_difference = SparkOperator(
     task_id="check_schema_difference",
-    name=pod_name,
+    name=POD_NAME,
     arguments=args,
-    namespace=namespace,
-    spark_class=main_class,
-    spark_jar=jar,
+    namespace=NAMESPACE,
+    spark_class=MAIN_CLASS,
+    spark_jar=JAR,
     spark_config="xsmall-etl",
     dag=dag
 )
 
+
 def format_slack_message(**kwargs):
+    """
+    Formats a Slack message to notify of missing tables.
+
+    :param kwargs:
+    :return: formatted message
+    """
     task_instance = kwargs["ti"]
     diff_result = task_instance.xcom_pull(task_ids="check_schema_difference", key="return_value")
     message = """
     :large_orange_circle: Missing Tables in Centro.\n
     """
     return message + "\n".join(str(x) for x in diff_result)
+
 
 send_to_slack = SlackWebhookOperator(
     task_id="send_to_slack",
