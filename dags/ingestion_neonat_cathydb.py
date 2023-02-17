66dad6c2734806194d0871b182513d8d40516a20
Laura Bégin <lbegin@ferlab.bio>
2023-02-17 13:48:45 -0500

    feat(neonat): Add start + end tasks (#173)

--- dags/ingestion_neonat_cathydb.py
+++ dags/ingestion_neonat_cathydb.py
@@ -5,6 +5,8 @@ DAG pour l'ingestion des data de neonat se trouvant dans cathydb
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models.param import Param
+from airflow.operators.empty import EmptyOperator
+
 from operators.spark import SparkOperator
 
 
@@ -36,81 +38,87 @@ dag = DAG(
     catchup=True
 )
 
-icca_external_numeric = SparkOperator(
-    task_id="raw_icca_external_numeric",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_external_numeric", '{{ds}}'],  # {{ds}} input date
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+with dag:
 
-icca_piicix_num = SparkOperator(
-    task_id="raw_icca_piicix_num",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_piicix_num", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+    start = EmptyOperator("start_ingestion_neonat_cathydb")
 
-icca_external_patient = SparkOperator(
-    task_id="raw_icca_external_patient",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_external_patient", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag)
+    icca_external_numeric = SparkOperator(
+        task_id="raw_icca_external_numeric",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_external_numeric", '{{ds}}'],  # {{ds}} input date
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
 
-icca_external_wave = SparkOperator(
-    task_id="raw_icca_external_wave",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_external_wave", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+    icca_piicix_num = SparkOperator(
+        task_id="raw_icca_piicix_num",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_piicix_num", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
 
-icca_piicix_sig = SparkOperator(
-    task_id="raw_icca_piicix_sig",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_piicix_sig", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+    icca_external_patient = SparkOperator(
+        task_id="raw_icca_external_patient",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_external_patient", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag)
 
-icca_piicix_sig_calibre = SparkOperator(
-    task_id="raw_icca_piicix_sig_calibre",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_piicix_sig_calibre", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+    icca_external_wave = SparkOperator(
+        task_id="raw_icca_external_wave",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_external_wave", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
 
-icca_piicix_alertes = SparkOperator(
-    task_id="raw_icca_piicix_alertes",
-    name=POD_NAME,
-    arguments=["config/prod.conf", "default", "raw_icca_piicix_alertes", '{{ds}}'],
-    namespace=NAMESPACE,
-    spark_class=MAIN_CLASS,
-    spark_jar=JAR,
-    spark_config="medium-etl",
-    dag=dag
-)
+    icca_piicix_sig = SparkOperator(
+        task_id="raw_icca_piicix_sig",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_piicix_sig", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
 
-[icca_external_numeric, icca_external_wave, icca_external_patient]
-icca_external_patient >> [icca_piicix_num, icca_piicix_sig, icca_piicix_sig_calibre, icca_piicix_alertes]
+    icca_piicix_sig_calibre = SparkOperator(
+        task_id="raw_icca_piicix_sig_calibre",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_piicix_sig_calibre", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
+
+    icca_piicix_alertes = SparkOperator(
+        task_id="raw_icca_piicix_alertes",
+        name=POD_NAME,
+        arguments=["config/prod.conf", "default", "raw_icca_piicix_alertes", '{{ds}}'],
+        namespace=NAMESPACE,
+        spark_class=MAIN_CLASS,
+        spark_jar=JAR,
+        spark_config="medium-etl",
+        dag=dag
+    )
+
+    end = EmptyOperator("publish_ingestion_neonat_cathydb")
+
+    start >> icca_external_patient >> [icca_piicix_num, icca_piicix_sig, icca_piicix_sig_calibre, icca_piicix_alertes] >> end
+    start >> [icca_external_numeric, icca_external_wave, icca_external_patient] >> end
