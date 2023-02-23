e0f4145a07cc0a8165d1ff81b3a09bedb4fbd811
zoemcl <zoemmclennan@gmail.com>
2023-02-23 13:07:52 -0500

    UNIC-1040: add start and success logs

--- dags/core/slack.py
+++ dags/core/slack.py
@@ -6,6 +6,8 @@ from airflow.models import Variable
 
 class Slack:
 
+    SUCCESS = ':large_green_square:'
+    INFO = ':large_blue_square:'
     ERROR = ':large_red_square:'
 
     base_url = Variable.get('base_url', None)
@@ -34,7 +36,7 @@ class Slack:
             *Log Url*: {log_url}
             """.format(
             exec_date=context['execution_date'],
-            log_url=context['task_instance'].log_url.replace("http://localhost:8080", Slack.base_url),
+            log_url=context['task_instance'].log_url,
         )
 
         dag_link = Slack._dag_link(
@@ -42,6 +44,18 @@ class Slack:
         )
 
         Slack.notify(f'Task {dag_link} failed.{slack_msg}', type=Slack.ERROR)
+
+    def notify_dag_start(context):
+        dag_id = context['dag'].dag_id
+        dag_link = Slack._dag_link(dag_id, dag_id, context['run_id'])
+        dag_exec_date = context['execution_date']
+        Slack.notify(f'DAG {dag_link} started at {dag_exec_date}', Slack.INFO)
+
+    def notify_dag_completion(context):
+        dag_id = context['dag'].dag_id
+        dag_link = Slack._dag_link(dag_id, dag_id, context['run_id'])
+        Slack.notify(f'DAG {dag_link} completed.', Slack.SUCCESS)
+
     def _dag_link(text: str, dag_id: str, run_id: str = '', task_id: str = ''):
         if Slack.base_url:
             params = urllib.parse.urlencode({
