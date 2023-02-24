e4511b61babb1ccddd73123f25063de8d9e17c13
zoemcl <zoemmclennan@gmail.com>
2023-02-24 11:20:18 -0500

    UNIC-1040: removing tiemout

--- dags/core/default_args.py
+++ dags/core/default_args.py
@@ -1,9 +1,6 @@
-def generate_default_args(owner, on_failure_callback, on_success_callback=None, on_execute_callback=None):
+def generate_default_args(owner, on_failure_callback):
     return {
         "owner": owner,
         "depends_on_past": False,
-        "execution_timeout": 3,
-        "on_failure_callback": on_failure_callback,
-        "on_success_callback": on_success_callback,
-        "on_execute_callback": on_execute_callback
+        "on_failure_callback": on_failure_callback
     }
