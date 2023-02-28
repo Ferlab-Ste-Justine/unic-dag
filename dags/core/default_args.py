f8de57ed30e6d693a71c8571a39072ef4151f966
zoemcl <zoemmclennan@gmail.com>
2023-02-28 12:28:02 -0500

    UNIC-1040: removing retries and dynamically setting execution_timeout

--- dags/core/default_args.py
+++ dags/core/default_args.py
@@ -1,10 +1,6 @@
-from datetime import timedelta
-
-
 def generate_default_args(owner, on_failure_callback):
     return {
         "owner": owner,
         "depends_on_past": False,
-        "on_failure_callback": on_failure_callback,
-        "execution_timeout": timedelta(hours=3)
+        "on_failure_callback": on_failure_callback
     }
