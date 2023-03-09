026b234ad7f41acf2c75c8145ec74a056a11f76c
Zoé McLennan <zoemmclennan@gmail.com>
2023-03-09 17:48:33 -0500

    UNIC-875: adding xcom push (#220)

--- dags/core/failure.py
+++ dags/core/failure.py
@@ -6,15 +6,18 @@ class Failure:
     def on_failure_callback(context):
         namespace = context['task'].namespace
         print(f"Operator Namespace: {namespace}")
-        name = context['task'].name
-        exception = context['exception']
+        # name = context['task'].name
+        # exception = context['exception']
         # name = context['task'].pod.metadata.name
         # print(f"Operator Name: {name}")
+        # name = context['task'].name
+        name = context['task'].pod.metadata.name
+        print(f"Operator Name: {name}")
 
-        #extract from exception
-        regex = name + "-.{32}"
-        pod_name = re.findall(regex, str(exception))[0]
-        print(f"Pod Name: {pod_name}")
+        # #extract from exception
+        # regex = name + "-.{32}"
+        # pod_name = re.findall(regex, str(Exception))[0]
+        # print(f"Pod Name: {pod_name}")
 
         Cleanup.cleanup_pods(name, namespace, is_failure=True)
         Slack.notify_task_failure(context)
