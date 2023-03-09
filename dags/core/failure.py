056ab5deec3fecae872cad241fa26bc215349a5c
Zoé McLennan <zoemmclennan@gmail.com>
2023-03-09 17:16:39 -0500

    Unic 875 (#218)
    * UNIC-875: revertin exec_timout_hours
    
    * UNIC-875: on_failure_callback pod deletion
    
    * UNIC-875-2: uppercase var for tests
    
    * unic-875-2: removing unused import
    
    * UNIC-875-2: removing unused vars
    
    * UNIC-875: extract from exception

--- dags/core/failure.py
+++ dags/core/failure.py
@@ -1,17 +1,19 @@
 from core.slack import Slack
 from core.cleanup import Cleanup
+import re
 
 class Failure:
     def on_failure_callback(context):
-        namespace = context['task'].pod.metadata.namespace
+        namespace = context['task'].namespace
         print(f"Operator Namespace: {namespace}")
-        name = context['task'].pod.metadata.name
-        print(f"Operator Name: {name}")
+        name = context['task'].name
+        # name = context['task'].pod.metadata.name
+        # print(f"Operator Name: {name}")
 
-        # #extract from exception
-        # regex = name + "-.{32}"
-        # pod_name = re.findall(regex, str(Exception))[0]
-        # print(f"Pod Name: {pod_name}")
+        #extract from exception
+        regex = name + "-.{32}"
+        pod_name = re.findall(regex, str(Exception))[0]
+        print(f"Pod Name: {pod_name}")
 
         Cleanup.cleanup_pods(name, namespace, is_failure=True)
         Slack.notify_task_failure(context)
