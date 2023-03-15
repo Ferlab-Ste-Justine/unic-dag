926fcc19fa7dd12fda854619763998ee57f5fd07
Zoé McLennan <zoemmclennan@gmail.com>
2023-03-15 13:52:27 -0400

    UNIC-875: reverting execution timeout (#234)

--- dags/core/slack.py
+++ dags/core/slack.py
@@ -12,6 +12,7 @@ class Slack:
     base_url = Variable.get('base_url', None)
     slack_hook_url = Variable.get('slack_hook_url', None)
 
+    @staticmethod
     def notify(markdown: str, type=INFO):
         if Slack.slack_hook_url:
             airflow_link = f' *[*<{Slack.base_url}|Airflow>*]*' if Slack.base_url else ''
@@ -26,6 +27,8 @@ class Slack:
                     },
                 ],
             })
+
+    @staticmethod
     def notify_task_failure(context):
         dag_id = context['dag'].dag_id
         task_id = context['task'].task_id
@@ -44,17 +47,20 @@ class Slack:
 
         Slack.notify(f'Task {dag_link} failed.{slack_msg}', type=Slack.ERROR)
 
+    @staticmethod
     def notify_dag_start(context):
         dag_id = context['dag'].dag_id
         dag_link = Slack._dag_link(dag_id, dag_id, context['run_id'])
         dag_exec_date = context['execution_date']
         Slack.notify(f'DAG {dag_link} started at {dag_exec_date}', Slack.INFO)
 
+    @staticmethod
     def notify_dag_completion(context):
         dag_id = context['dag'].dag_id
         dag_link = Slack._dag_link(dag_id, dag_id, context['run_id'])
         Slack.notify(f'DAG {dag_link} completed.', Slack.SUCCESS)
 
+    @staticmethod
     def _dag_link(text: str, dag_id: str, run_id: str = '', task_id: str = ''):
         if Slack.base_url:
             params = urllib.parse.urlencode({
@@ -64,6 +70,7 @@ class Slack:
             return f'<{Slack.base_url}/dags/{dag_id}/grid?{params}|{text}>'
         return text
 
+    @staticmethod
     def http_post(url: str, json: Any = None) -> requests.Response:
         with requests.post(url, json=json) as response:
             response.raise_for_status()
