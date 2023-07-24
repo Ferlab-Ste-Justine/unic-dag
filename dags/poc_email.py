960031e1f0db0ca40922cfc423bc35d8398a96fc
Laura Bégin <lbegin@ferlab.bio>
2023-07-24 10:17:39 -0400

    poc: UNIC-1135 Set poc_email schedule_interval to None (#316)

--- dags/poc_email.py
+++ dags/poc_email.py
@@ -11,6 +11,7 @@ from sqlalchemy_utils.types.enriched_datetime.pendulum_date import pendulum
 dag = DAG(
     dag_id="poc_email",
     start_date=datetime(2023, 7, 14, 0, tzinfo=pendulum.timezone("America/Montreal")),
+    schedule_interval=None,
     is_paused_upon_creation=True,
     tags=["poc"]
 )
