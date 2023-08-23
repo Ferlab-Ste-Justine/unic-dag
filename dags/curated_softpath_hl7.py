48871543d3d0afe96a93913e97cc6333bbf2187b
zoemcl <zoemmclennan@gmail.com>
2023-08-23 16:16:27 -0400

    UNIC-1067: set catchup to false

--- dags/curated_softpath_hl7.py
+++ dags/curated_softpath_hl7.py
@@ -41,7 +41,7 @@ dag = DAG(
     dagrun_timeout=timedelta(hours=2),
     default_args=args,
     is_paused_upon_creation=True,
-    catchup=True,
+    catchup=False,
     max_active_runs=1,
     max_active_tasks=1,
     tags=["curated"]
