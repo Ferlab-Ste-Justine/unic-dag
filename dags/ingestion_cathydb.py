fa41a5bad060034373b52859e38603e9357c9813
Jaafar Amine <pycodemaker@gmail.com>
2023-10-06 15:07:11 -0400

    zap: chore: update dag conf

--- dags/ingestion_cathydb.py
+++ dags/ingestion_cathydb.py
@@ -43,8 +43,8 @@ dag = DAG(
     default_args=args,
     is_paused_upon_creation=True,
     catchup=True,
-    max_active_runs=2,
-    max_active_tasks=3,
+    max_active_runs=1,
+    max_active_tasks=2,
     tags=["ingestion"]
 )
 
