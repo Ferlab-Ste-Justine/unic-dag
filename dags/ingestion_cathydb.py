46af39da8eb6e975ed0066479450b95ede12368e
Jaafar Amine <pycodemaker@gmail.com>
2023-10-06 15:16:57 -0400

    zap: chore: revert max_active_runs parm

--- dags/ingestion_cathydb.py
+++ dags/ingestion_cathydb.py
@@ -43,7 +43,7 @@ dag = DAG(
     default_args=args,
     is_paused_upon_creation=True,
     catchup=True,
-    max_active_runs=1,
+    max_active_runs=2,
     max_active_tasks=2,
     tags=["ingestion"]
 )
