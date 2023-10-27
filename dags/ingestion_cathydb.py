b6571687e5af9f4360e7af461344d6705244a7e9
Jaafar Amine <pycodemaker@gmail.com>
2023-10-27 14:26:23 -0400

    :zap: set cluster to xlarge for wave & numeric

--- dags/ingestion_cathydb.py
+++ dags/ingestion_cathydb.py
@@ -64,7 +64,7 @@ with dag:
         spark_class=MAIN_CLASS,
         spark_jar=jar,
         spark_failure_msg=spark_failure_msg,
-        spark_config="large-etl",
+        spark_config="xlarge-etl",
         dag=dag
     )
 
@@ -88,7 +88,7 @@ with dag:
         spark_class=MAIN_CLASS,
         spark_jar=jar,
         spark_failure_msg=spark_failure_msg,
-        spark_config="large-etl",
+        spark_config="xlarge-etl",
         dag=dag
     )
 
