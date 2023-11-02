e02a9fd3e3ba8d52221878388d222e78fa56b9c9
Zoé McLennan <zoemmclennan@gmail.com>
2023-11-02 15:40:49 -0400

    :rocket: (feat): add anonymized HL7 (#454)
    * :rocket: (feat): add anonymized HL7
    
    * :green_heart: (fix): remove unused import
    
    * :wrench: (chore): add start date and timedelta hl7

--- dags/curated_softpath_hl7.py
+++ dags/curated_softpath_hl7.py
@@ -5,6 +5,7 @@ DAG pour le parsing des messages HL7 de Softpath
 from datetime import datetime, timedelta
 
 import pendulum
+
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
@@ -15,7 +16,7 @@ from operators.spark import SparkOperator
 DOC = """
 # Curated Softpath HL7 DAG
 
-ETL curated pour parser les messages HL&7 softpath déposé en zone rouge
+ETL curated pour parser les messages HL7 softpath déposé en zone rouge
 
 ### Description
 Cet ETL roule pour parser les messages HL7 et les convertir de messages .hl7 au format Delta. 
@@ -25,8 +26,10 @@ La run du 2 janvier 2020 parse les données du 1 janvier dans le lac.
 
 """
 
-ZONE = "red"
-MAIN_CLASS = "bio.ferlab.ui.etl.red.curated.hl7.Main"
+ANONYMIZED_ZONE = "yellow"
+CURATED_ZONE = "red"
+ANONYMIZED_CLASS = "bio.ferlab.ui.etl.yellow.anonymized"
+CURATED_CLASS = "bio.ferlab.ui.etl.red.curated.hl7.Main"
 args = default_args.copy()
 args.update({
     'provide_context': True,
@@ -36,15 +39,15 @@ args.update({
 dag = DAG(
     dag_id="curated_softpath_hl7",
     doc_md=DOC,
-    start_date=datetime(2023, 7, 13, 1, tzinfo=pendulum.timezone("America/Montreal")),
+    start_date=datetime(2023, 9, 27, 7, tzinfo=pendulum.timezone("America/Montreal")),
     schedule_interval=timedelta(days=1),
     params=default_params,
     dagrun_timeout=timedelta(hours=2),
     default_args=args,
     is_paused_upon_creation=True,
-    catchup=True,
+    catchup=False, # change back to True after initial tests
     max_active_runs=1,
-    max_active_tasks=1,
+    max_active_tasks=2,
     tags=["curated"]
 )
 
@@ -58,8 +61,20 @@ with dag:
         task_id="curated_softpath_hl7_oru_r01",
         name="curated-softpath-hl7-oru-r01",
         arguments=["config/prod.conf", "initial", "curated_softpath_hl7_oru_r01", '{{ds}}'],  # {{ds}} input date
-        zone=ZONE,
-        spark_class=MAIN_CLASS,
+        zone=CURATED_ZONE,
+        spark_class=CURATED_CLASS,
+        spark_jar=jar,
+        spark_failure_msg=spark_failure_msg,
+        spark_config="small-etl",
+        dag=dag
+    )
+
+    anonymized_softpath_hl7_oru_r01 = SparkOperator(
+        task_id="anonymized_softpath_hl7_oru_r01",
+        name="anonymized-softpath-hl7-oru-r01",
+        arguments=["config/prod.conf", "initial", "anonymized_softpath_hl7_oru_r01"],
+        zone=ANONYMIZED_ZONE,
+        spark_class=ANONYMIZED_CLASS,
         spark_jar=jar,
         spark_failure_msg=spark_failure_msg,
         spark_config="small-etl",
@@ -71,4 +86,4 @@ with dag:
         on_success_callback=Slack.notify_dag_completion
     )
 
-    start >> curated_softpath_hl7_oru_r01 >> end
+    start >> curated_softpath_hl7_oru_r01 >> anonymized_softpath_hl7_oru_r01 >> end
