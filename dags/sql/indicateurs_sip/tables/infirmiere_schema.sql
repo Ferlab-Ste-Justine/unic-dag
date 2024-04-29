f9e848dc3f615c6e4ac616162bb4de9eb4765fda
zoemcl <zoemmclennan@gmail.com>
2024-04-29 13:36:01 -0400

    :sparkles: (feat) add lits and infirmiere tables to postgresql

---
+++ dags/sql/indicateurs_sip/tables/infirmiere_schema.sql
@@ -0,0 +1,11 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+-- create infirmiere table
+CREATE TABLE IF NOT EXISTS indicateurs_sip.infirmiere (
+    day Date,
+    dayNurseCount Int,
+    dayPatientCount Int,
+    eveningNurseCount Int,
+    eveningPatientCount Int,
+    nightNurseCount Timestamp,
+    nightPatientCount Int);
