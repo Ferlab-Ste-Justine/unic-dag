c6dc8759f390a0c6c4ffb5bc458884c3f10c065c
sallyalomar <55108807+sallyalomar@users.noreply.github.com>
2024-05-02 11:02:22 -0400

    UNIC-1483: fix du schema ventilation (#700)

--- dags/sql/indicateurs_sip/tables/ventilation_schema.sql
+++ dags/sql/indicateurs_sip/tables/ventilation_schema.sql
@@ -3,6 +3,6 @@
 -- create ventilation table
 CREATE TABLE IF NOT EXISTS indicateurs_sip.ventilation (
     encounterId Int,
-    ventilationDate Date,
+    ventilationDateTime Timestamp,
     ventilationType VARCHAR);
 
