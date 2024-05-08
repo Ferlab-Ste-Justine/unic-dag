95a9026af95010c4e6bd18ff52f6aa87464ac231
Laura Bégin <lbegin@ferlab.bio>
2024-05-08 14:03:40 -0400

    ::pencil2: fix: UNIC-1460 Use correct schema_creation_sql_path key (#709)

--- dags/lib/tasks/postgres.py
+++ dags/lib/tasks/postgres.py
@@ -9,7 +9,7 @@ def create_schema(sql_config: dict) -> PostgresCaOperator:
     return PostgresCaOperator(
         task_id=f"create_{sql_config['schema']['name']}_schema",
         postgres_conn_id="postgresql_bi_rw",
-        sql=sql_config['schema']['postgres_table_creation_sql_path'],
+        sql=sql_config['schema']['postgres_schema_creation_sql_path'],
         ca_path=postgres_ca_path,
         ca_filename=postgres_ca_filename,
         ca_cert=postgres_ca_cert
