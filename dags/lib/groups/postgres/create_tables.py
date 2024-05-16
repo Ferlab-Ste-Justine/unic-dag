c16cdff47ddb3dc47bcc93a68ebc6b7cb71766da
Laura Bégin <lbegin@ferlab.bio>
2024-05-16 16:16:41 -0400

    :sparkles: feat: UNIC-1436 Manage task dependencies in create_tables group

--- dags/lib/groups/postgres/create_tables.py
+++ dags/lib/groups/postgres/create_tables.py
@@ -5,16 +5,25 @@ from lib.postgres import postgres_bi_ca_path, postgres_ca_filename, postgres_bi_
 
 
 @task_group(group_id="create_tables")
-def create_tables(sql_config: dict, conn_id: str = postgres_bi_conn_id, ca_path: str = postgres_bi_ca_path, ca_cert: str = postgres_bi_ca_cert):
+def create_tables(sql_config: dict, conn_id: str = postgres_bi_conn_id, ca_path: str = postgres_bi_ca_path,
+                  ca_cert: str = postgres_bi_ca_cert):
     """
     Create tables task group.
     """
-    [PostgresCaOperator(
-        task_id=f"create_{table_config['name']}_table",
-        postgres_conn_id=conn_id,
-        sql=table_config['postgres_table_creation_sql_path'],
-        ca_path=ca_path,
-        ca_filename=postgres_ca_filename,
-        ca_cert=ca_cert,
-        skip=skip_task(table_config['name'])
-    ) for table_config in sql_config['tables']]
+    jobs = {}
+    for table_config in sql_config['tables']:
+        table_name = table_config['name']
+        job = PostgresCaOperator(
+            task_id=f"create_{table_name}_table",
+            postgres_conn_id=conn_id,
+            sql=table_config['postgres_table_creation_sql_path'],
+            ca_path=ca_path,
+            ca_filename=postgres_ca_filename,
+            ca_cert=ca_cert,
+            skip=skip_task(table_name))
+
+        jobs[table_name] = {'task': job, 'dependencies': table_config['dependencies']}
+
+    for table_name, job in jobs.items():
+        for dependency in job['dependencies']:
+            jobs[dependency]['task'] >> job['task']
--- dags/postgres_indicateurs_sip.py
+++ dags/postgres_indicateurs_sip.py
@@ -32,12 +32,12 @@ paramètre au DAG seront dropées et recréées selon les schémas définis dans
 sql_config = {
     "schema": {"name" : "indicateurs_sip", "postgres_schema_creation_sql_path" : "sql/indicateurs_sip/schema.sql"},
     "tables": [
-        {"name": "catheter"   , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/catheter_schema.sql"},
-        {"name": "extubation" , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/extubation_schema.sql"},
-        {"name": "sejour"     , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/sejour_schema.sql"},
-        {"name": "ventilation", "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/ventilation_schema.sql"},
-        {"name": "lits"       , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/lits_schema.sql"},
-        {"name": "infirmieres", "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/infirmieres_schema.sql"}
+        {"name": "catheter"   , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/catheter_schema.sql"   , "dependencies": []},
+        {"name": "extubation" , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/extubation_schema.sql" , "dependencies": []},
+        {"name": "sejour"     , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/sejour_schema.sql"     , "dependencies": []},
+        {"name": "ventilation", "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/ventilation_schema.sql", "dependencies": []},
+        {"name": "lits"       , "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/lits_schema.sql"       , "dependencies": []},
+        {"name": "infirmieres", "postgres_table_creation_sql_path": "sql/indicateurs_sip/tables/infirmieres_schema.sql", "dependencies": []}
     ]
 }
 table_name_list = [table['name'] for table in sql_config['tables']]
