a4e304bccff4c9e60d610e50b0cad158ec01d8e5
Laura Bégin <lbegin@ferlab.bio>
2024-05-17 11:01:02 -0400

    :card_file_box: fix(catalog): UNIC-1436 Add schema prefix to custom enums

--- dags/sql/catalog/tables/variable.sql
+++ dags/sql/catalog/tables/variable.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS catalog.variable
     last_update          TIMESTAMP                          NOT NULL,
     name                 VARCHAR(255)                       NOT NULL,
     path                 VARCHAR(255) UNIQUE                NOT NULL,
-    value_type           value_type_enum                    NOT NULL,
+    value_type           catalog.value_type_enum            NOT NULL,
     label_fr             VARCHAR(255)                       NOT NULL,
     label_en             VARCHAR(255)                       NOT NULL,
     value_set_id         INTEGER REFERENCES value_set (id),
     from_variable_id     INTEGER[],
     derivation_algorithm VARCHAR(500),
     notes                VARCHAR(255),
-    variable_status      status_enum                        NOT NULL,
-    rolling_version      rolling_version_enum               NOT NULL,
+    variable_status      catalog.status_enum                NOT NULL,
+    rolling_version      catalog.rolling_version_enum       NOT NULL,
     to_be_published      BOOLEAN                            NOT NULL
 );
