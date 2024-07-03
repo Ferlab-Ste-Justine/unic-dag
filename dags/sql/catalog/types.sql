25317db3a96f0b4d84f9b3d4aa73ae6e34c91175
Laura Bégin <lbegin@ferlab.bio>
2024-07-03 11:16:42 -0400

    :card_file_box: refactor(catalog): UNIC-1435 Remove resource_project from resource_type_enum

--- dags/sql/catalog/types.sql
+++ dags/sql/catalog/types.sql
@@ -3,7 +3,7 @@
 DO
 $$
     BEGIN
-        CREATE TYPE catalog.resource_type_enum AS ENUM ('warehouse', 'research_project', 'resource_project', 'eqp', 'source_system');
+        CREATE TYPE catalog.resource_type_enum AS ENUM ('warehouse', 'research_project', 'eqp', 'source_system');
         CREATE TYPE catalog.value_type_enum AS ENUM ('integer', 'boolean', 'string', 'decimal', 'date', 'datetime');
         CREATE TYPE catalog.entity_type_enum AS ENUM ('patient', 'observation', 'diagnosis', 'medication', 'procedure', 'episode', 'encounter', 'delivery', 'pregnancy', 'practitioner', 'location');
         CREATE TYPE catalog.domain_type_enum AS ENUM ('transfusion', 'imaging', 'medication', 'pathology', 'microbiology', 'laboratory', 'sociodemographics', 'diagnosis', 'pregnancy', 'medical_history');
