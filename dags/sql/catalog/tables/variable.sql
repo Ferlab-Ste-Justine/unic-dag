43ad81f6be02007e42389fa5896b4c4176ff8885
Laura Bégin <lbegin@ferlab.bio>
2024-09-05 12:46:13 -0400

    :sparkles: feat(catalog.variable): UNIC-1603 Add from_source_system_id col

--- dags/sql/catalog/tables/variable.sql
+++ dags/sql/catalog/tables/variable.sql
@@ -3,20 +3,21 @@
 -- create variable table
 CREATE TABLE IF NOT EXISTS catalog.variable
 (
-    id                   SERIAL PRIMARY KEY,
-    table_id             INTEGER REFERENCES catalog.dict_table (id) ON DELETE CASCADE NOT NULL,
-    last_update          TIMESTAMP                                                    NOT NULL DEFAULT NOW(),
-    created_at           TIMESTAMP                                                    NOT NULL DEFAULT now(),
-    name                 VARCHAR(255)                                                 NOT NULL,
-    path                 VARCHAR(255) UNIQUE                                          NOT NULL,
-    value_type           catalog.value_type_enum,
-    label_fr             VARCHAR(255),
-    label_en             VARCHAR(255),
-    value_set_id         INTEGER                                                      REFERENCES catalog.value_set (id) ON DELETE SET NULL,
-    from_variable_id     INTEGER[],
-    derivation_algorithm TEXT,
-    notes                VARCHAR(255),
-    variable_status      catalog.status_enum,
-    rolling_version      catalog.rolling_version_enum                                 NOT NULL,
-    to_be_published      BOOLEAN                                                      NOT NULL
+    id                    SERIAL PRIMARY KEY,
+    table_id              INTEGER REFERENCES catalog.dict_table (id) ON DELETE CASCADE NOT NULL,
+    last_update           TIMESTAMP                                                    NOT NULL DEFAULT NOW(),
+    created_at            TIMESTAMP                                                    NOT NULL DEFAULT now(),
+    name                  VARCHAR(255)                                                 NOT NULL,
+    path                  VARCHAR(255) UNIQUE                                          NOT NULL,
+    value_type            catalog.value_type_enum,
+    label_fr              VARCHAR(255),
+    label_en              VARCHAR(255),
+    value_set_id          INTEGER                                                      REFERENCES catalog.value_set (id) ON DELETE SET NULL,
+    from_variable_id      INTEGER[],
+    from_source_system_id INTEGER[],
+    derivation_algorithm  TEXT,
+    notes                 VARCHAR(255),
+    variable_status       catalog.status_enum,
+    rolling_version       catalog.rolling_version_enum                                 NOT NULL,
+    to_be_published       BOOLEAN                                                      NOT NULL
 );
