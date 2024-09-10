c8ce0680483b4b075e7fc2bb9868232e6fc08b32
zoemcl <zoemmclennan@gmail.com>
2024-09-10 12:43:03 -0400

    :bug: (fix) debug optimize_delta_tables params'

--- dags/optimize_delta_tables.py
+++ dags/optimize_delta_tables.py
@@ -57,7 +57,7 @@ with DAG(
 
     def arguments(number_of_versions: str, app_name: str) -> List[str]:
         args = [
-            "--number-of-versions", '10',
+            "--number-of-versions", "\"10\"",
             "--config", "config/prod.conf",
             "--steps", "default",
             "--app-name", app_name
