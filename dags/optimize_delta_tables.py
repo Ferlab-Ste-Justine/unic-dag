7f3c32864ed2e0bb0532209ff9d686a02b3f09f8
Zoé McLennan <zoemmclennan@gmail.com>
2024-09-10 14:53:06 -0400

    :bug: (fix) debug optimize_delta_tables params (#876)

--- dags/optimize_delta_tables.py
+++ dags/optimize_delta_tables.py
@@ -1,7 +1,7 @@
 """
 DAG pour l'optimization des tables deltas
 """
-# pylint: disable=missing-function-docstring, invalid-name, expression-not-assigned, too-many-ancestors, unused-argument
+# pylint: disable=missing-function-docstring, invalid-name, expression-not-assigned, too-many-ancestors
 
 from datetime import datetime
 
@@ -57,7 +57,7 @@ with DAG(
 
     def arguments(number_of_versions: str, app_name: str) -> List[str]:
         args = [
-            "--number-of-versions", "\"10\"",
+            "--number-of-versions", number_of_versions,
             "--config", "config/prod.conf",
             "--steps", "default",
             "--app-name", app_name
