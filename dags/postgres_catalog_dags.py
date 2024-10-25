21580e98c68babc334e1b144c3daf7ab223a0745
Laura Bégin <lbegin@ferlab.bio>
2024-10-25 15:27:32 -0400

    :recycle: refactor: UNIC-1645 Generate catalog dag for each env

---
+++ dags/postgres_catalog_dags.py
@@ -0,0 +1,82 @@
+"""
+Génération des DAGSs pour la création des tables dans la bd unic_db pour le catalogue de l'UnIC.
+Un DAG par environnement postgres est généré.
+"""
+# pylint: disable=missing-function-docstring, expression-not-assigned
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.models import Param
+from airflow.utils.trigger_rule import TriggerRule
+
+from lib.groups.postgres.create_tables import create_tables
+from lib.postgres import postgres_vlan2_ca_path, postgres_vlan2_ca_cert, \
+    PostgresEnv, unic_postgres_vlan2_conn_id
+from lib.slack import Slack
+from lib.tasks.notify import start, end
+from lib.tasks.postgres import create_resource, PostgresResource
+
+
+# Specify path to SQL query for schema and each table
+sql_config = {
+    "schema": {"name": "catalog", "postgres_schema_creation_sql_path": "sql/catalog/schema.sql"},
+    "types": {"name": "catalog", "postgres_types_creation_sql_path": "sql/catalog/types.sql"},
+    "tables": [
+        {"name": "resource"      , "postgres_table_creation_sql_path": "sql/catalog/tables/resource.sql"      , "dependencies": ["analyst"]},
+        {"name": "analyst"       , "postgres_table_creation_sql_path": "sql/catalog/tables/analyst.sql"       , "dependencies": []},
+        {"name": "value_set"     , "postgres_table_creation_sql_path": "sql/catalog/tables/value_set.sql"     , "dependencies": []},
+        {"name": "dict_table"    , "postgres_table_creation_sql_path": "sql/catalog/tables/dict_table.sql"    , "dependencies": ["resource"]},
+        {"name": "variable"      , "postgres_table_creation_sql_path": "sql/catalog/tables/variable.sql"      , "dependencies": ["dict_table", "value_set"]},
+        {"name": "value_set_code", "postgres_table_creation_sql_path": "sql/catalog/tables/value_set_code.sql", "dependencies": ["value_set"]},
+        {"name": "mapping"       , "postgres_table_creation_sql_path": "sql/catalog/tables/mapping.sql"       , "dependencies": ["value_set_code"]},
+        {"name": "user"          , "postgres_table_creation_sql_path": "sql/catalog/tables/user.sql"          , "dependencies": []},
+        {"name": "refresh_token" , "postgres_table_creation_sql_path": "sql/catalog/tables/refresh_token.sql" , "dependencies": ["user"]}
+    ],
+    "indexes": {"name": "catalog", "postgres_indexes_creation_sql_path": "sql/catalog/indexes.sql"}
+}
+table_name_list = [table['name'] for table in sql_config['tables']]
+
+for env in PostgresEnv:
+    env_name = env.value
+    conn_id = unic_postgres_vlan2_conn_id(env)
+
+    doc = f"""
+    # Postgres **{env_name}** Catalog DAG
+    
+    DAG pour la création des tables dans la bd `unic_db` pour le catalogue de l'UnIC dans l'environnement **{env_name}**.
+    
+    ### Description
+    Ce DAG crée le schéma `catalog` et ses types dans la base de données `unic_db`. Ensuite, les tables passées en
+    paramètre au DAG seront dropées et recréées selon les schémas définis dans `sql/catalog`. Finalement, des indexes sont
+    créés sur les tables.
+    
+    ### Configuration
+    * Paramètre `tables` : Liste des tables à créer dans la base de données. Par défaut, crée toutes les tables.
+    """
+
+    with DAG(
+            dag_id=f"postgres_{env_name}_catalog",
+            params={
+                "tables": Param(table_name_list, type=['array'], examples=table_name_list),
+            },
+            default_args={
+                'trigger_rule': TriggerRule.NONE_FAILED,
+                'on_failure_callback': Slack.notify_task_failure,
+            },
+            doc_md=doc,
+            start_date=datetime(2024, 5, 13),
+            is_paused_upon_creation=False,
+            schedule_interval=None,
+            max_active_tasks=1,
+            tags=["postgresql"]
+    ) as dag:
+        start("start_postgres_catalog") \
+            >> create_resource(PostgresResource.SCHEMA, sql_config, conn_id=conn_id, ca_path=postgres_vlan2_ca_path,
+                               ca_cert=postgres_vlan2_ca_cert) \
+            >> create_resource(PostgresResource.TYPES, sql_config, conn_id=conn_id, ca_path=postgres_vlan2_ca_path,
+                               ca_cert=postgres_vlan2_ca_cert) \
+            >> create_tables(sql_config, conn_id=conn_id, ca_path=postgres_vlan2_ca_path, ca_cert=postgres_vlan2_ca_cert) \
+            >> create_resource(PostgresResource.INDEXES, sql_config, conn_id=conn_id, ca_path=postgres_vlan2_ca_path,
+                               ca_cert=postgres_vlan2_ca_cert) \
+            >> end("end_postgres_catalog")
---
+++ dags/postgres_catalog_load_metadata_from_excel_dags.py
@@ -0,0 +1,264 @@
+"""
+Génération des DAGs pour le chargement dans le Catalogue des métadonnées se trouvant dans des fichiers Excel.
+Un DAG par environnement postgres est généré.
+"""
+# pylint: disable=missing-function-docstring, invalid-name, expression-not-assigned
+
+from datetime import datetime
+from typing import List, Optional, Union
+
+from airflow import DAG
+from airflow.decorators import task, task_group
+from airflow.exceptions import AirflowFailException
+from airflow.models import Param, DagRun
+from airflow.utils.trigger_rule import TriggerRule
+
+from lib.config import jar, spark_failure_msg, yellow_minio_conn_id
+from lib.operators.spark import SparkOperator
+from lib.operators.upsert_csv_to_postgres import UpsertCsvToPostgres
+from lib.postgres import skip_task, postgres_vlan2_ca_path, postgres_ca_filename, \
+    postgres_vlan2_ca_cert, PostgresEnv, unic_postgres_vlan2_conn_id
+from lib.slack import Slack
+from lib.tasks.excel import excel_to_csv
+from lib.tasks.notify import start, end
+
+ZONE = "yellow"
+MAIN_CLASS = "bio.ferlab.ui.etl.catalog.csv.Main"
+YELLOW_BUCKET = "yellow-prd"
+table_name_list = ["resource", "analyst", "dict_table", "value_set", "value_set_code", "variable", "mapping"]
+env_name = None
+conn_id = None
+
+
+def get_project() -> str:
+    return "{{ params.project or '' }}"
+
+
+def arguments(table_name: str, app_name: str, project_name: Optional[str] = None) -> \
+        List[str]:
+    args = [
+        table_name,
+        "--config", "config/prod.conf",
+        "--steps", "default",
+        "--app-name", app_name,
+        "--env", env_name
+    ]
+    if project_name is not None:
+        args.append("--project")
+        args.append(project_name)
+
+    return args
+
+
+for env in PostgresEnv:
+    env_name = env.value
+    conn_id = unic_postgres_vlan2_conn_id(env)
+
+    doc = f"""
+    # Postgres **{env_name}** Load Metadata from Excel DAG
+    
+    DAG pour le chargement dans le Catalogue **{env_name}** des métadonnées se trouvant dans des fichiers Excel.
+    
+    ### Description
+    Ce DAG convertit les fichiers Excel en fichier CSV pour qu'ils puissent être traités par Spark. Ensuite, les tables
+    passées en paramètre au DAG seront chargées dans la BD Postgres du Catalogue dans l'environnement **{env_name}**.
+    
+    ### Configuration
+    * Paramètre `branch` : Branche du jar à utiliser.
+    * Paramètre `tables` : Liste des tables à créer dans la base de données. Par défaut, crée toutes les tables.
+    * Paramètre `project` : Nom du projet à charger. Obligatoire si `dict_table`, `value_set`, `value_set_code`, `variable` ou `mapping` font partie des tables sélectionnées.
+    
+    ## Tables à charger
+    * analyst : Charge la table `analyst`.
+    * resource : Charge la table `resource`.
+    * dict_table : Charge la table `dict_table`.
+    * value_set : Charge la table `value_set`.
+    * value_set_code : Charge la table `value_set_code`.
+    * variable : Charge la table `variable`.
+    * mapping : Charge la table `mapping`.
+    """
+
+    with DAG(
+            dag_id=f"postgres_{env_name}_catalog_load_metadata_from_excel",
+            params={
+                "branch": Param("master", type="string"),
+                "tables": Param(default=table_name_list, type=["array"], examples=table_name_list,
+                                description="Tables to load."),
+                "project": Param(None, type=["null", "string"],
+                                 description="Required if 'dict_table', 'value_set', 'value_set_code', 'variable' or 'mapping' are selected in 'tables' param."),
+            },
+            default_args={
+                'trigger_rule': TriggerRule.NONE_FAILED,
+                'on_failure_callback': Slack.notify_task_failure,
+            },
+            doc_md=doc,
+            start_date=datetime(2024, 6, 12),
+            is_paused_upon_creation=False,
+            schedule_interval=None,
+            tags=["postgresql"]
+    ) as dag:
+        @task(task_id="get_tables")
+        def get_tables(ti=None) -> List[str]:
+            dag_run: DagRun = ti.dag_run
+            return dag_run.conf["tables"]
+
+
+        @task(task_id="validate_project_param")
+        def validate_project_param(tables: List[str], project: str):
+            if any(table in tables for table in
+                   ["dict_table", "value_set", "value_set_code", "variable", "mapping"]) and project == "":
+                raise AirflowFailException(
+                    "DAG param 'project' is required when tables other than 'resource' and 'analyst' are selected.")
+
+
+        @task_group(group_id="excel_to_csv")
+        def excel_to_csv_group():
+            def excel_to_csv_task(table_name: str, s3_source_key: Optional[str] = None,
+                                  s3_destination_key: Optional[str] = None, sheet_name: Union[str, int] = 0):
+                return excel_to_csv.override(task_id=f"excel_to_csv_{table_name}")(
+                    s3_source_bucket=YELLOW_BUCKET,
+                    s3_source_key=s3_source_key,
+                    s3_destination_bucket=YELLOW_BUCKET,
+                    s3_destination_key=s3_destination_key,
+                    s3_conn_id=yellow_minio_conn_id,
+                    sheet_name=sheet_name,
+                    skip=skip_task(table_name)
+                )
+
+            excel_to_csv_task("analyst",
+                              s3_source_key="catalog/analyst.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/output/analyst.csv")  # Directly put analyst in output directory since no transformations have to be run by the ETL
+            excel_to_csv_task("resource",
+                              s3_source_key="catalog/resource.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/resource.csv")
+            excel_to_csv_task("dict_table",
+                              s3_source_key=f"catalog/dictionary_{get_project()}.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/{get_project()}/dict_table.csv",
+                              sheet_name="dict_table")
+            excel_to_csv_task("variable",
+                              s3_source_key=f"catalog/dictionary_{get_project()}.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/{get_project()}/variable.csv",
+                              sheet_name="variable")
+            excel_to_csv_task("value_set",
+                              s3_source_key=f"catalog/dictionary_{get_project()}.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/{get_project()}/value_set.csv",
+                              sheet_name="value_set")
+            excel_to_csv_task("value_set_code",
+                              s3_source_key=f"catalog/dictionary_{get_project()}.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/{get_project()}/value_set_code.csv",
+                              sheet_name="value_set_code")
+            excel_to_csv_task("mapping",
+                              s3_source_key=f"catalog/dictionary_{get_project()}.xlsx",
+                              s3_destination_key=f"catalog/{env_name}/csv/input/{get_project()}/mapping.csv",
+                              sheet_name="mapping")
+
+
+        @task_group(group_id="load_tables")
+        def load_tables_group():
+            def prepare_table(table_name: str, cluster_size: str, project_name: Optional[str] = None) -> SparkOperator:
+                if project_name:
+                    args = arguments(table_name, app_name=f"prepare_{table_name}_table_for_{project_name}",
+                                     project_name=project_name)
+
+                else:
+                    args = arguments(table_name, app_name=f"prepare_{table_name}_table")
+
+                return SparkOperator(
+                    task_id=f"prepare_{table_name}_table",
+                    name=f"prepare-{table_name}-table",
+                    arguments=args,
+                    zone=ZONE,
+                    spark_class=MAIN_CLASS,
+                    spark_jar=jar,
+                    spark_failure_msg=spark_failure_msg,
+                    spark_config=cluster_size,
+                    skip=skip_task(table_name),
+                )
+
+            def load_table(table_name: str, s3_key: str, primary_keys: List[str]):
+                return UpsertCsvToPostgres(
+                    task_id=f"load_{table_name}_table",
+                    s3_bucket="yellow-prd",
+                    s3_key=s3_key,
+                    s3_conn_id=yellow_minio_conn_id,
+                    postgres_conn_id=conn_id,
+                    postgres_ca_path=postgres_vlan2_ca_path,
+                    postgres_ca_filename=postgres_ca_filename,
+                    postgres_ca_cert=postgres_vlan2_ca_cert,
+                    schema_name="catalog",
+                    table_name=table_name,
+                    table_schema_path=f"sql/catalog/tables/{table_name}.sql",
+                    primary_keys=primary_keys,
+                    excluded_columns=["created_at"],
+                    skip=skip_task(table_name)
+                )
+
+            # analyst table (directly loaded since no transformations have to be run by the ETL)
+            load_analyst_table_task = load_table("analyst",
+                                                 s3_key="catalog/csv/output/analyst/analyst.csv",
+                                                 primary_keys=["name"])
+
+            # resource table
+            prepare_resource_table_task = prepare_table("resource", cluster_size="xsmall-etl")
+            load_resource_table_task = load_table("resource",
+                                                  s3_key="catalog/csv/output/resource/resource.csv",
+                                                  primary_keys=["code"])
+
+            prepare_resource_table_task >> load_resource_table_task
+            load_analyst_table_task >> prepare_resource_table_task
+
+            @task_group(group_id="load_project_tables")
+            def load_project_tables():
+                # value_set table
+                prepare_value_set_table_task = prepare_table("value_set", cluster_size="xsmall-etl",
+                                                             project_name=get_project())
+                load_value_set_table_task = load_table("value_set",
+                                                       s3_key=f"catalog/csv/output/{get_project()}/value_set/value_set.csv",
+                                                       primary_keys=["name"])
+
+                # dict_table table
+                prepare_dict_table_table_task = prepare_table("dict_table", cluster_size="xsmall-etl",
+                                                              project_name=get_project())
+                load_dict_table_table_task = load_table("dict_table",
+                                                        s3_key=f"catalog/csv/output/{get_project()}/dict_table/dict_table.csv",
+                                                        primary_keys=["resource_id", "name"])
+
+                # value_set_code table
+                prepare_value_set_code_table_task = prepare_table("value_set_code", cluster_size="small-etl",
+                                                                  project_name=get_project())
+                load_value_set_code_table_task = load_table("value_set_code",
+                                                            s3_key=f"catalog/csv/output/{get_project()}/value_set_code/value_set_code.csv",
+                                                            primary_keys=["value_set_id", "code"])
+
+                prepare_variable_table_task = prepare_table("variable", cluster_size="small-etl",
+                                                            project_name=get_project())
+                load_variable_table_task = load_table("variable",
+                                                      s3_key=f"catalog/csv/output/{get_project()}/variable/variable.csv",
+                                                      primary_keys=["path"])
+
+                # mapping table
+                prepare_mapping_table_task = prepare_table("mapping", cluster_size="small-etl",
+                                                           project_name=get_project())
+                load_mapping_table_task = load_table("mapping",
+                                                     s3_key=f"catalog/csv/output/{get_project()}/mapping/mapping.csv",
+                                                     primary_keys=["value_set_code_id", "original_value"])
+
+                prepare_value_set_table_task >> load_value_set_table_task
+                prepare_dict_table_table_task >> load_dict_table_table_task
+                prepare_value_set_code_table_task >> load_value_set_code_table_task
+                prepare_variable_table_task >> load_variable_table_task
+                prepare_mapping_table_task >> load_mapping_table_task
+
+                load_resource_table_task >> prepare_dict_table_table_task
+                load_value_set_table_task >> prepare_value_set_code_table_task
+                load_value_set_code_table_task >> prepare_mapping_table_task
+                [load_dict_table_table_task, load_value_set_table_task] >> prepare_variable_table_task
+
+            load_resource_table_task >> load_project_tables()
+
+
+        get_tables_task = get_tables()
+        start() >> get_tables_task >> validate_project_param(tables=get_tables_task, project=get_project()) \
+            >> excel_to_csv_group() \
+            >> load_tables_group() \
+            >> end()
