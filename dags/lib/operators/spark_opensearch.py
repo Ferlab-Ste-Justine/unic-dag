a18f56f5955eafe9807f985e569f46c9e3d94aa9
zoemcl <zoemmclennan@gmail.com>
2024-11-22 15:46:00 -0500

    :recycle: (feat) update spark opensearch operator

--- dags/lib/operators/spark_opensearch.py
+++ dags/lib/operators/spark_opensearch.py
@@ -1,22 +1,28 @@
+import subprocess
+
 from airflow.exceptions import AirflowSkipException
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from kubernetes.client import models as k8s
 
-from lib.cleanup import Cleanup
+from lib.operators.spark import SparkOperator
 
 
-class SparkOpenSearchOperator(KubernetesPodOperator):
-    template_fields = KubernetesPodOperator.template_fields + (
+class SparkOpenSearchOperator(SparkOperator):
+    template_fields = SparkOperator.template_fields + (
+        'spark_class',
         'spark_jar',
+        'spark_failure_msg',
+        'zone',
+        'spark_config',
         'skip'
     )
-
     def __init__(
             self,
             spark_class: str,
             spark_jar: str,
             spark_failure_msg: str,
             zone: str,
+            ca_path: str,
+            ca_filename: str,
+            ca_cert: str,
             spark_config: str = '',
             skip: bool = False,
             **kwargs,
@@ -33,92 +39,33 @@ class SparkOpenSearchOperator(KubernetesPodOperator):
         self.spark_class = spark_class
         self.spark_jar = spark_jar
         self.spark_failure_msg = spark_failure_msg
-        self.namespace = f'unic-{zone}'
+        self.zone = zone
         self.spark_config = spark_config
+        self.ca_path = ca_path
+        self.ca_filename = ca_filename
+        self.ca_cert = ca_cert
         self.skip = skip
+        super().__init__(
+            spark_class=self.spark_class,
+            spark_jar=self.spark_jar,
+            spark_failure_msg=self.spark_failure_msg,
+            zone=self.zone,
+            spark_config=self.spark_config,
+            **kwargs
+        )
 
     def execute(self, **kwargs):
         if self.skip:
             raise AirflowSkipException()
 
-        self.cmds = ['/opt/client-entrypoint.sh']
-        self.image_pull_policy = 'IfNotPresent'
-
-        self.env_vars = [
-            k8s.V1EnvVar(
-                name='SPARK_CLIENT_POD_NAME',
-                value_from=k8s.V1EnvVarSource(
-                    field_ref=k8s.V1ObjectFieldSelector(
-                        field_path='metadata.name',
-                    ),
-                ),
-            ),
-            k8s.V1EnvVar(
-                name='SPARK_JAR',
-                value=self.spark_jar,
-            ),
-            k8s.V1EnvVar(
-                name='SPARK_CLASS',
-                value=self.spark_class,
-            ),
-        ]
-
-        self.volumes = [
-            k8s.V1Volume(
-                name='spark-defaults',
-                config_map=k8s.V1ConfigMapVolumeSource(
-                    name='spark-defaults',
-                ),
-            ),
-            k8s.V1Volume(
-                name='spark-s3-credentials',
-                secret=k8s.V1SecretVolumeSource(
-                    secret_name='spark-s3-credentials',
-                ),
-            ),
-            k8s.V1Volume(
-                name='opensearch-admin-credentials',
-                secret=k8s.V1SecretVolumeSource(
-                    secret_name='unic-prod-opensearch-admin-credentials',
-                ),
-            ),
-        ]
-        self.volume_mounts = [
-            k8s.V1VolumeMount(
-                name='spark-defaults',
-                mount_path='/opt/spark-configs/defaults',
-                read_only=True,
-            ),
-            k8s.V1VolumeMount(
-                name='spark-s3-credentials',
-                mount_path='/opt/spark-configs/s3-credentials',
-                read_only=True,
-            ),
-            k8s.V1VolumeMount(
-                name='opensearch-admin-credentials',
-                mount_path='/opt/opensearch-credentials',
-                read_only=True,
-            ),
-        ]
-
-        if self.spark_config:
-            self.volumes.append(
-                k8s.V1Volume(
-                    name=self.spark_config,
-                    config_map=k8s.V1ConfigMapVolumeSource(
-                        name=self.spark_config,
-                    ),
-                ),
-            )
-            self.volume_mounts.append(
-                k8s.V1VolumeMount(
-                    name=self.spark_config,
-                    mount_path=f'/opt/spark-configs/{self.spark_config}',
-                    read_only=True,
-                ),
-            )
-
+        self.load_cert()
         super().execute(**kwargs)
 
-        Cleanup.cleanup_pods(self.pod.metadata.name, self.pod.metadata.namespace, self.spark_failure_msg)
+    def load_cert(self):
+        subprocess.run(["mkdir", "-p", self.ca_path])
+
+        with open(self.ca_path + self.ca_filename, "w") as outfile:
+            outfile.write(self.ca_cert)
+
+        subprocess.run(["sh", "-c", f"keytool -importkeystore -noprompt -srckeystore /etc/pki/java/cacerts -destkeystore /opt/keystores/truststore.p12 -srcstoretype PKCS12 -deststoretype PKCS12 -srcstorepass changeit -storepass changeit && keytool -import -noprompt -keystore /opt/keystores/truststore.p12 -file {self.ca_path}{self.ca_filename} -storepass changeit -alias es"])
 
