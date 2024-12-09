47085b6d43c3e2002f2293848d31b5ce50e97a87
zoemcl <zoemmclennan@gmail.com>
2024-12-09 16:15:59 -0500

    :adhesive_bandage: (fix) use os instead of es

--- dags/lib/operators/spark_opensearch.py
+++ dags/lib/operators/spark_opensearch.py
@@ -28,14 +28,6 @@ class SparkOpenSearchOperator(SparkOperator):
             skip: Optional[bool] = False,
             **kwargs,
     ) -> None:
-        self.spark_class = spark_class
-        self.spark_jar = spark_jar
-        self.spark_failure_msg = spark_failure_msg
-        self.zone = zone
-        self.spark_config = spark_config
-        self.os_credentials_secret_name = os_credentials_secret_name
-        self.os_cert_secret_name = os_cert_secret_name
-        self.skip = skip
         super().__init__(
             spark_class=self.spark_class,
             spark_jar=self.spark_jar,
@@ -44,7 +36,14 @@ class SparkOpenSearchOperator(SparkOperator):
             spark_config=self.spark_config,
             **kwargs
         )
-
+        self.spark_class = spark_class
+        self.spark_jar = spark_jar
+        self.spark_failure_msg = spark_failure_msg
+        self.zone = zone
+        self.spark_config = spark_config
+        self.os_credentials_secret_name = os_credentials_secret_name
+        self.os_cert_secret_name = os_cert_secret_name
+        self.skip = skip
 
     def execute(self, **kwargs):
         if self.skip:
@@ -69,14 +68,14 @@ class SparkOpenSearchOperator(SparkOperator):
             ),
         ]
 
-        if self.env_vars and self.es_credentials_secret_name:
+        if self.env_vars and self.os_credentials_secret_name:
             self.env_vars += new_env_vars
         else:
             self.env_vars = new_env_vars
 
         new_volumes = [
             k8s.V1Volume(
-                name=self.es_cert_secret_name,
+                name=self.os_cert_secret_name,
                 secret=k8s.V1SecretVolumeSource(
                     secret_name='unic-prod-opensearch-ca-certificate',
                     default_mode=0o555
@@ -84,20 +83,20 @@ class SparkOpenSearchOperator(SparkOperator):
             )
         ]
 
-        if self.volumes and self.es_cert_secret_name:
+        if self.volumes and self.os_cert_secret_name:
             self.volumes += new_volumes
         else:
             self.volumes = new_volumes
 
         new_volume_mounts = [
             k8s.V1VolumeMount(
-                name=self.es_cert_secret_name,
+                name=self.os_cert_secret_name,
                 mount_path='/opt/os-ca',
                 read_only=True,
             )
         ]
 
-        if self.volume_mounts and self.es_cert_secret_name:
+        if self.volume_mounts and self.os_cert_secret_name:
             self.volume_mounts += new_volume_mounts
         else:
             self.volume_mounts = new_volume_mounts
