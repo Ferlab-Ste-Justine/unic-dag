fbf7aa8729ede2e3bc65316d2433e1a8a9e0e5a3
zoemcl <zoemmclennan@gmail.com>
2025-01-20 17:07:56 -0500

    :bug: (fix) set centro qa cluster to large

--- dags/lib/groups/qa/tests.py
+++ dags/lib/groups/qa/tests.py
@@ -10,7 +10,8 @@ from lib.tasks import qa
 @task_group(group_id="tests")
 def tests(test_confs: List[Dict], resource: str, zone: str, subzone: str,
           config_file: str, jar: str, dag: DAG) -> TaskGroup:
-    for pre_test in test_confs:
-        pre_test_name = pre_test['name']
-        pre_test_destinations = pre_test['destinations']
-        qa.test(pre_test_name, pre_test_destinations, resource, zone, subzone, config_file, jar, dag)
+    for test in test_confs:
+        test_name = test['name']
+        test_destinations = test['destinations']
+        test_cluster_type = test['cluster_type']
+        qa.test(test_name, test_destinations, resource, zone, subzone, config_file, jar, dag, cluster_type = test_cluster_type)
