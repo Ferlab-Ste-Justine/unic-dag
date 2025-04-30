849b127a4ca419be4cd394a18eff2a006ea8f06f
zoemcl <zoemmclennan@gmail.com>
2025-04-30 14:03:44 -0400

    :wrench: (fix) update os secret names

--- dags/lib/opensearch.py
+++ dags/lib/opensearch.py
@@ -25,8 +25,8 @@ os_cert_filename = 'ca.crt'
 
 # Opensearch prod configs
 os_prod_url = 'https://workers.opensearch.unic.sainte-justine.intranet'
-os_prod_credentials_secret = 'opensearch-prod-dags-credentials'
-os_prod_cert_secret = 'opensearch-prod-ca-certificate'
+os_prod_credentials_secret = 'opensearch-dags-credentials'
+os_prod_cert_secret = 'unic-prod-opensearch-ca-certificate'
 os_prod_username = Variable.get('os_prod_username', None)
 os_prod_password = Variable.get('os_prod_password', None)
 os_prod_cert = Variable.get('os_prod_ca_certificate', None)
@@ -35,7 +35,7 @@ os_prod_cert_path = '/tmp/ca/os/prod/'
 # Opensearch qa configs
 os_qa_url = 'https://workers.opensearch.qa.unic.sainte-justine.intranet'
 os_qa_credentials_secret = 'opensearch-qa-dags-credentials'
-os_qa_cert_secret = 'opensearch-qa-ca-certificate'
+os_qa_cert_secret = 'unic-prod-opensearch-qa-ca-certificate'
 os_qa_username = Variable.get('os_qa_username', None)
 os_qa_password = Variable.get('os_qa_password', None)
 os_qa_cert = Variable.get('os_qa_ca_certificate', None)
