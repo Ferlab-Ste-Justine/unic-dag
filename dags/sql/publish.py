897293a231afd16127301aaa85abaafeda7b98d0
Zoé McLennan <zoemmclennan@gmail.com>
2025-05-26 16:38:32 -0400

    unicweb-12 (#1264)
    * :adhesive_bandage: fix: UNICWEB-15 update resource_query publish
    
    * :adhesive_bandage: fix: UNICWEB-15 update resource query

--- dags/sql/publish.py
+++ dags/sql/publish.py
@@ -4,8 +4,8 @@ def resource_query(resource_code: str) -> str:
     Query to get resource information for a given resource code.
     """
     return f"""
-        SELECT * FROM catalog.resource 
-        WHERE code='{resource_code}'
+        SELECT * FROM catalog.resource r
+        WHERE r.code='{resource_code}'
         AND r.to_be_published = true
     """
 
