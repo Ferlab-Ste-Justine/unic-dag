f8500f310ac1e41c86037fefbeb28d21f93f1198
Zoé McLennan <zoemmclennan@gmail.com>
2025-06-12 17:25:32 -0400

    :adhesive_bandage: (fix) os publish alias check (#1299)

--- dags/lib/tasks/opensearch.py
+++ dags/lib/tasks/opensearch.py
@@ -145,17 +145,21 @@ def publish_index(env_name: str, release_id: str, alias: str) -> None:
 
     new_index = f"{alias}_{release_id}"
 
-    alias_info = os_client.indices.get_alias(name=alias, ignore_unavailable=True)
-    logging.info(f"Alias info: {alias_info}")
-    current_index = next(iter(alias_info), "")
+    alias_exists = os_client.indices.exists_alias(name=alias)
+    logging.info(f"Alias '{alias}' exists: {alias_exists}")
+
+    actions = []
+    if alias_exists:
+        alias_info = os_client.indices.get_alias(name=alias)
+        current_index = next(iter(alias_info), "")
+        actions.append({"remove": {"index": current_index, "alias": alias}})
+    else:
+        current_index = "No alias"
+
+    actions.append({"add": {"index": new_index, "alias": alias}})
 
     logging.info(f"Current Index: {current_index}")
     logging.info(f"New Index: {new_index}")
-
-    actions = [
-        {"remove": {"index": current_index, "alias": alias, "must_exist": False}},
-        {"add": {"index": new_index, "alias": alias}}
-    ]
 
     try:
         response = os_client.indices.update_aliases(body={"actions": actions})
