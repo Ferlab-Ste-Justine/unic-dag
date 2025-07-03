ef0c5fa9f4366ad527feb53b9abecaa498d4e005
Robert C <rcaterev@gmail.com>
2025-07-03 12:41:32 -0400

    🐛 fix(publish): Import constants inside virutal env

--- dags/lib/groups/publish/publish.py
+++ dags/lib/groups/publish/publish.py
@@ -3,7 +3,7 @@ from airflow.decorators import task_group
 from lib.opensearch import OpensearchAlias, OpensearchEnv
 from lib.tasks.opensearch import prepare_index, load_index, publish_index, get_next_release_id
 from lib.tasks.notify import start, end
-from lib.config import PUBLISHED_BUCKET, GREEN_MINIO_CONN_ID
+from lib.config import PUBLISHED_BUCKET, YELLOW_MINIO_CONN_ID
 
 from lib.tasks.publish import get_resource_code, get_version_to_publish, get_include_dictionary, update_dict_current_version, publish_dictionary, get_publish_kwargs
 from lib.tasks.excel import parquet_to_excel
@@ -16,6 +16,6 @@ def publish_research_project(pg_conn_id: str, resource_code: str, version_to_pub
     >> parquet_to_excel.override(task_id="publish_project_data").expand_kwargs(get_publish_kwargs(
         resource_code=resource_code,
         version_to_publish=version_to_publish,
-        minio_conn_id=GREEN_MINIO_CONN_ID,
+        minio_conn_id=YELLOW_MINIO_CONN_ID,
         bucket=PUBLISHED_BUCKET
     ))
